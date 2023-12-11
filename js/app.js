f5a07cc81ef3333a75d5cc7dc50d3dcab8bfa6a6
kevinmitchell36 <kevingm31@gmail.com>
2023-12-11 11:55:21 -0600

    Add filter and reset capability
    User can filter workouts and meals, reset the day, and set total calorie limit

--- js/app.js
+++ js/app.js
@@ -48,6 +48,19 @@ class CalorieTracker {
     }
   }
 
+  reset() {
+    this._totalCalories = 0;
+    this._meals = [];
+    this._workouts = [];
+    this._render();
+  }
+
+  setLimit(calorieLimit) {
+    this._calorieLimit = calorieLimit;
+    this._displayCaloriesLimit();
+    this._render();
+  }
+
   // Private methods
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById('calories-total');
@@ -113,7 +126,6 @@ class CalorieTracker {
         </button>
       </div>
     </div>`;
-    console.log(mealEl);
     mealsEl.appendChild(mealEl);
   }
 
@@ -150,6 +162,7 @@ class CalorieTracker {
 
 }
 
+
 class Meal {
   constructor(name, calories) {
     this.id = Math.random().toString(16).slice(2);
@@ -157,6 +170,7 @@ class Meal {
     this.calories = calories;
   }
 }
+
 
 class Workout {
   constructor(name, calories) {
@@ -166,6 +180,7 @@ class Workout {
   }
 }
 
+
 class App {
   constructor() {
     this._tracker = new CalorieTracker();
@@ -173,6 +188,10 @@ class App {
     document.getElementById('workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'));
     document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this, 'meal'));
     document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'));
+    document.getElementById('filter-meals').addEventListener('keyup', this._filterItems.bind(this, 'meal'));
+    document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'));
+    document.getElementById('reset').addEventListener('click', this._reset.bind(this));
+    document.getElementById('limit-form').addEventListener('submit', this._setLimit.bind(this));
   }
 
   _newItem(type, e) {
@@ -207,6 +226,44 @@ class App {
       type === 'meal' ? this._tracker.removeMeal(id) : this._tracker.removeWorkout(id)
       e.target.closest('.card').remove()
     }
+  }
+
+  _filterItems(type, e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll(`#${type}-items .card`).forEach( (item) => {
+      const name = item.firstElementChild.firstElementChild.textContent;
+      if (name.toLowerCase().indexOf(text) !== -1) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    })
+  }
+
+  _reset(e) {
+    this._tracker.reset();
+    document.getElementById('meal-items').innerHTML = '';
+    document.getElementById('workout-items').innerHTML = '';
+    document.getElementById('filter-meals').value = '';
+    document.getElementById('filter-workouts').value = '';
+
+  }
+
+  _setLimit(e) {
+    e.preventDefault();
+    const limit = document.getElementById('limit');
+
+    if (limit.value === '') {
+      alert('Please enter a number');
+      return
+    }
+
+    this._tracker.setLimit(+limit.value);
+    limit.value = '';
+
+    const modalEl = document.getElementById('limit-modal');
+    const modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
   }
 }
 
